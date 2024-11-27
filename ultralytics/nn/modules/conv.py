0d03e3329b66338131695e68de37101d4de66a43
Ennyyz <ngocyen2812.pham@gmail.com>
2024-11-27 10:16:49 +0700

    add ECA

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -340,39 +340,24 @@ def channel_shuffle(x, groups=2):  ##shuffle channel
     return out
 
 
-class GAM_Attention(nn.Module):
-    def __init__(self, c1, c2, group=True, rate=4):
-        super(GAM_Attention, self).__init__()
+class ECAAttention(nn.Module):
+    """Constructs a ECA module.
+    Args:
+        channel: Number of channels of the input feature map
+        k_size: Adaptive selection of kernel size
+    """
 
-        self.channel_attention = nn.Sequential(
-            nn.Linear(c1, int(c1 / rate)),
-            nn.ReLU(inplace=True),
-            nn.Linear(int(c1 / rate), c1)
-        )
-
-        self.spatial_attention = nn.Sequential(
-
-            nn.Conv2d(c1, c1 // rate, kernel_size=7, padding=3, groups=rate) if group else nn.Conv2d(c1, int(c1 / rate),
-                                                                                                     kernel_size=7,
-                                                                                                     padding=3),
-            nn.BatchNorm2d(int(c1 / rate)),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(c1 // rate, c2, kernel_size=7, padding=3, groups=rate) if group else nn.Conv2d(int(c1 / rate), c2,
-                                                                                                     kernel_size=7,
-                                                                                                     padding=3),
-            nn.BatchNorm2d(c2)
-        )
+    def __init__(self, c1, k_size=3):
+        super(ECAAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        b, c, h, w = x.shape
-        x_permute = x.permute(0, 2, 3, 1).view(b, -1, c)
-        x_att_permute = self.channel_attention(x_permute).view(b, h, w, c)
-        x_channel_att = x_att_permute.permute(0, 3, 1, 2)
-        # x_channel_att=channel_shuffle(x_channel_att,4) #last shuffle
-        x = x * x_channel_att
+        # feature descriptor on the global spatial information
+        y = self.avg_pool(x)
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+        # Multi-scale information fusion
+        y = self.sigmoid(y)
 
-        x_spatial_att = self.spatial_attention(x).sigmoid()
-        x_spatial_att = channel_shuffle(x_spatial_att, 4)  # last shuffle
-        out = x * x_spatial_att
-        # out=channel_shuffle(out,4) #last shuffle
-        return out
+        return x * y.expand_as(x)
